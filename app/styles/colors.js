42a41c81cd6d95a047b1a0671b79eadb93535b85
ngachakanai <113998341+martins6131@users.noreply.github.com>
2025-08-08 21:19:47 +0300

    Update colors.js

--- app/styles/colors.js
+++ app/styles/colors.js
@@ -1,1 +1,10 @@
+// app/styles/colors.js
+export default {
+  black: '#000000',
+  primerGreen: '#34C759',
+  white: '#FFFFFF',
+  darkGray: '#111214',
+  softGray: '#F2F4F7',
+  accent: '#1F7A3A'
+};
 
